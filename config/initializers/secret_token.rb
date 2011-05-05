b19169e28820192c9b873c5856030a6dcdeb9d24
Eric Hill <eric@synthesizer.org>
2011-05-04 17:44:38 -0700

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taskerino::Application.config.secret_token = '0b015f8e891f90d5d606db5d5c19472dabe35b54c8594a7e93c7076083c8149aa1e98f027911b85d30a50397a11198e8409574251e08c6e96a829b3242a85117'
